6887a7a368385dc0fc77b054346275b22c91e162
Akshay Tikapache <akshay.pache@gmail.com>
2024-07-19 19:22:57 +0530

    project file

---
+++ src/main/java/com/devops/StarAgile/KubernetesassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.devops.StarAgile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesassignmentApplication.class, args);
+	}
+
+}
